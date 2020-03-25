619317ea9c3d0bc178ace14801381f0b8f07a14a
lilywu <Lily.Wu@auo.com>
2020-03-25 15:47:23 +0800

    Add Donut order

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-global.orderId = 2;
+global.orderId = 3;
 
 // Dashboard
 router.get('/', (req, res) =>{
